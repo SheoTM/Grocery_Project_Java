fc69c8279dab52ba88c070e8433d13ec7cb6d277
Leon <leon0411@op.pl>
2025-05-19 16:45:57 +0200

    Initial commit

---
+++ src/test/java/org/example/grocery_project/GroceryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grocery_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
